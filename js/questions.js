a01762b3d21092e9e120ca7ce6eb4a1e77842551
Aurimas Butvilauskas <a.butvilauskas@gmail.com>
2021-04-29 18:20:05 +0300

    Initial commit

---
+++ js/questions.js
@@ -0,0 +1,225 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "How old _____ you?",
+    answer: "are",
+    options: [
+      "is",
+      "am",
+      "are",
+      "do"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Ten plus twelve is _____ .",
+    answer: "twenty-two",
+    options: [
+      "twenty-twelve",
+      "twenty-two",
+      "twenty-second",
+      "twenty-too"
+    ]
+  },
+    {
+    numb: 3,
+    question: "I saw three small _____ .",
+    answer: "childrens",
+    options: [
+      "children's",
+      "children",
+      "childs",
+      "childrens"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Lucy _____ like dancing.",
+    answer: "doesn't",
+    options: [
+      "isn't",
+      "doesn't",
+      "don't",
+      "haven't"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Excuse me, _____ time is it?",
+    answer: "what",
+    options: [
+      "why",
+      "when",
+      "which",
+      "what"
+    ]
+  },
+  {
+    numb: 6,
+    question: "_____ live in China.",
+    answer: "The Chinese",
+    options: [
+      "The Chinese",
+      "The China",
+      "The Chinish",
+      "The Chin's"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What's _____ name?",
+    answer: "her",
+    options: [
+      "hier",
+      "hear",
+      "hair",
+      "her"
+    ]
+  },
+  {
+    numb: 8,
+    question: "_____ are three computers in my school.",
+    answer: "There",
+    options: [
+      "They",
+      "Their",
+      "There",
+      "These"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What time is it? Look at your _____.",
+    answer: "watch",
+    options: [
+      "witch",
+      "watch",
+      "wash",
+      "wish"
+    ]
+  },
+  {
+    numb: 10,
+    question: "I have my English classes _____ Monday.",
+    answer: "every",
+    options: [
+      "ever",
+      "never",
+      "every",
+      "each"
+    ]
+  },
+  {
+    numb: 11,
+    question: "I didn't _____ coffee yesterday.",
+    answer: "drank",
+    options: [
+      "drinked",
+      "drank",
+      "drink",
+      "drunk"
+    ]
+  },
+  {
+    numb: 12,
+    question: "This isn't your jacket. It's _____ .",
+    answer: "mine",
+    options: [
+      "mine",
+      "me",
+      "my",
+      "myself"
+    ]
+  },
+  {
+    numb: 13,
+    question: "Andy doesn’t want to go to the beach, because he _____ swim.",
+    answer: "can't",
+    options: [
+      "do",
+      "musn't",
+      "didn't",
+      "can't"
+    ]
+  },
+  {
+    numb: 14,
+    question: "_____ sugar do you put in your tea?",
+    answer: "How much",
+    options: [
+      "How many",
+      "How much",
+      "How any",
+      "Any much"
+    ]
+  },
+  {
+    numb: 15,
+    question: "Are you going to the café? _____ with you.",
+    answer: "I'll come",
+    options: [
+      "I'll come",
+      "I'm come",
+      "I come",
+      "I go"
+    ]
+  },
+  {
+    numb: 16,
+    question: "Can you _____ a photo of us, please?",
+    answer: "take",
+    options: [
+      "do",
+      "make",
+      "bake",
+      "take"
+    ]
+  },
+  {
+    numb: 17,
+    question: "I hate waiting _____ people who are late.",
+    answer: "for",
+    options: [
+      "for",
+      "of",
+      "to",
+      "at"
+    ]
+  },
+  {
+    numb: 18,
+    question: "This exercise is not difficult. It's very _____!",
+    answer: "easy",
+    options: [
+      "ease",
+      "easy",
+      "esy",
+      "eased"
+    ]
+  },
+  {
+    numb: 19,
+    question: "Laura is a movie star. She is an _____.",
+    answer: "actress",
+    options: [
+      "acting",
+      "actor",
+      "actress",
+      "act"
+    ]
+  },
+  {
+    numb: 20,
+    question: "Hurry up ow we'll _____ our train!",
+    answer: "miss",
+    options: [
+      "cancel",
+      "fail",
+      "leave",
+      "miss"
+    ]
+  },
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+];
